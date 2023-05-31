112f09451cc7e29007c182c7591a9d601f920d3a
@hexlet/cli <support@hexlet.io>
2023-05-31 08:51:08 +0300

    submit ruby-basics-ru/testing

--- ruby-basics-ru/testing/test/stack_test.rb
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -6,26 +6,31 @@ require_relative '../lib/stack'
 class StackTest < Minitest::Test
   # BEGIN
   def test_push
-    stack = Stack.new
-    stack.push!('ruby')
-    assert { stack.to_a == ['ruby'] }
+    stack = Stack.new %w[one two]
+    stack.push!('uno')
+    stack.push!('trezo')
+    assert { stack.to_a == ['one', 'two', 'uno', 'trezo'] }
+    assert { stack.size == 4}
   end
 
   def test_pop
-    stack = Stack.new(['ruby'])
+    stack = Stack.new %w[ruby js go c]
     stack.pop!
-    assert { stack.to_a == [] }
+    stack.pop!
+    assert { stack.to_a == ['ruby', 'js'] }
+    assert { stack.size == 2 }
   end
 
   def test_clear
     stack = Stack.new(['ruby', 'hash'])
     stack.clear!
-    assert { stack.to_a == [] }
+    assert { stack.empty? }
   end
 
   def test_emptiness
     stack = Stack.new
     assert { stack.empty? }
+    assert { stack.to_a == [] }
   end
   # END
 end
