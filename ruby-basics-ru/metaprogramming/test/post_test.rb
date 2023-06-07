d1a211677c0f367698c2937a0d89447d1f2cba5c
@hexlet/cli <support@hexlet.io>
2023-06-07 13:16:51 +0300

    submit ruby-basics-ru/metaprogramming

--- ruby-basics-ru/metaprogramming/test/post_test.rb
+++ ruby-basics-ru/metaprogramming/test/post_test.rb
@@ -15,16 +15,16 @@ class PostTest < Minitest::Test
     @expected_attributes = @attributes.merge(created_at: DateTime.parse(@attributes[:created_at]))
   end
 
-  # def test_model_contains_attributes_on_initialize
-  #   post = Post.new(@attributes)
-  #
-  #   assert { @expected_attributes[:id] == post.id }
-  #   assert { @expected_attributes[:title] == post.title }
-  #   assert { @expected_attributes[:body] == post.body }
-  #   assert { @expected_attributes[:created_at] == post.created_at }
-  #   assert { @expected_attributes[:published] == post.published }
-  #   assert { @expected_attributes == post.attributes }
-  # end
+  def test_model_contains_attributes_on_initialize
+    post = Post.new(@attributes)
+
+    assert { @expected_attributes[:id] == post.id }
+    assert { @expected_attributes[:title] == post.title }
+    assert { @expected_attributes[:body] == post.body }
+    assert { @expected_attributes[:created_at] == post.created_at }
+    assert { @expected_attributes[:published] == post.published }
+    assert { @expected_attributes == post.attributes }
+  end
 
   def test_model_contains_attributes_with_accessors
     post = Post.new
